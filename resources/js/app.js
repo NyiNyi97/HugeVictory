4392843f57aff0f4badcc551d7ba6404ab2f4dff
NyiNyi97 <warhaha450@gmail.com>
2022-11-14 10:14:42 +0630

    updated file

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,34 @@ $(document).ready(function () {
 	// 	}
 	// });
 
+	var swiper = new Swiper(".mySwiper", {
+		slidesPerView: 3,
+		spaceBetween: 30,
+		loop: true,
+		centerSlide: 'true',
+		fade: 'true',
+		grabCursor: 'true',
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		},
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+		breakpoints: {
+			0: {
+				slidesPerView: 1
+			},
+			520: {
+				slidesPerView: 2
+			},
+			950: {
+				slidesPerView: 3
+			}
+		}
+	});
+
 	$('.customer-logos').slick({
 		slidesToShow: 6,
 		slidesToScroll: 1,
@@ -37,6 +65,4 @@ $(document).ready(function () {
 		$(this).siblings().removeClass('active');
 		$(this).addClass('active');
 	});
-
-
 });
